0408ef5f90ba0866223e556b85ca54191e482798
swarupashinde04 <swarupa8387@gmail.com>
2026-01-09 18:52:33 +0530

    Create prime-no-checker.py

---
+++ prime-no-checker.py
@@ -0,0 +1,10 @@
+n = int(input("enter a number : "))
+if n<=1 :
+    print("not a prime number")
+else :
+    for i in range (2,n) :
+        if n%i == 0 :
+            print( "not a prime number")
+            break
+        else:
+            print("prime number")
