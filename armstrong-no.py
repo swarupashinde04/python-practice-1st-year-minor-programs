57df39bf58db4ef0283e0f53a57a80d268436791
Swarupa Shinde <swarupa8387@gmail.com>
2026-07-25 18:04:33 +0530

    Update armstrong-no.py

--- armstrong-no.py
+++ armstrong-no.py
@@ -1,7 +1,7 @@
 num = int(input("Enter a 3-digit number: "))
 temp = num
 sum = 0
-while temp > 0:
+while temp > 0: 
     digit = temp % 10
     sum = sum + digit * digit * digit
     temp = temp // 10
